29628dfba553daeedf9f2bd85c24fdebb3b61d41
appiappelt@gmail.com <appiappelt@gmail.com@0147436e-aed1-11de-b8dd-ab4b6ce6d521>
2014-03-10 18:51:30 +0000

    improved way of checking if logged on

--- modules/lessons/actions/homekhan.act.php
+++ modules/lessons/actions/homekhan.act.php
@@ -18,16 +18,20 @@
 	//----------------------------------------------------------------------------------------------
 	//	check if user is already logged in into KA Lite, otherwise automatically create user (1st time) and log in
 	//----------------------------------------------------------------------------------------------
-	if (true == $kapenta->session->has('c_sessionid') and '' !== $kapenta->session->get('c_sessionid')) {
+	$url = 'http://localhost/api/status';
+	$reply = $kapenta->utils->curlGet($url, '', false);
+	if (0 < strpos($reply, '"is_logged_in": true')) {
+//	if (true == $kapenta->session->has('c_sessionid') and '' !== $kapenta->session->get('c_sessionid')) {
 		//signed in already, continue below
 		//echo "We are logged in with KhanLite already!<br/>\n";
 	} else {
-		echo "We are not logged in with KhanLite!<br/>\n";
+//		echo "We are not logged in with KhanLite!<br/>\n";
+		logoutKhanLite();
 		createAndLoginKhanLite();
 	} 
 
 	//----------------------------------------------------------------------------------------------
-	//	call GET /coachreports from KA Lite Server
+	//	call GET /watch and exercise from KA Lite Server
 	//----------------------------------------------------------------------------------------------
 	$sessionid = $kapenta->session->get('c_sessionid');
 	$raw = $kapenta->utils->curlGet($kalite, '', false, 'sessionid='.$sessionid);

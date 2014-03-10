f323f7da6b8143e6fa601862671c771df0e67f36
awarenetdev@gmail.com <awarenetdev@gmail.com@0147436e-aed1-11de-b8dd-ab4b6ce6d521>
2014-03-10 06:35:38 +0000

    fix kalite settings block

--- modules/lessons/views/settings.block.php
+++ modules/lessons/views/settings.block.php
@@ -1,7 +1,33 @@
 <? /*
 
-<hr/>
-%%addPresetForm%%
-<hr/>
+<h2>KA Lite</h2>
+
+<form name='frmKALiteSettings' method='POST' action='%%serverPath%%lessons/settings/'>
+<input type='hidden' name='action' value='addPreset' />
+<table noborder>
+  <tr>
+    <td><b>KALite User</b></td>
+    <td><input type='text' name='kalite_admin_user' value='%%kalite.admin.user%%' style='width: 100%' /></td>
+  </tr>
+  <tr>
+    <td><b>KALite Pass</b></td>
+    <td><input type='password' name='kalite_admin_pwd' value='%%kalite.admin.pwd%%' style='width: 100%' /></td>
+  </tr>
+  <tr>
+    <td><b>KALite Installation</b></td>
+    <td><input type='text' name='kalite_installation' value='%%kalite.installation%%' style='width: 100%' /></td>
+  </tr>
+  <tr>
+    <td><b>KALite DB File</b></td>
+    <td><input type='text' name='kalite_db_file' value='%%kalite.db.file%%' style='width: 100%' /></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td><input type='submit' value='Update' /></td>
+  </tr>
+</table>
+</form>
+<br/>
+<div class='foot'></div>
 
 */ ?>
--- modules/lessons/views/settings.fn.php
+++ modules/lessons/views/settings.fn.php
@@ -12,22 +12,21 @@ function lessons_settings($args) {
 	//----------------------------------------------------------------------------------------------
 	//	check user role
 	//----------------------------------------------------------------------------------------------
-	if ('admin' != $user->role) { return ''; }
+	if ('admin' !== $kapenta->user->role) { return '(not logged in)'; }
 
 	//----------------------------------------------------------------------------------------------
 	//	make the block
 	//----------------------------------------------------------------------------------------------
-	$block = $theme->loadBlock('modules/lessons/views/settings.block.php');
+	$block = $kapenta->theme->loadBlock('modules/lessons/views/settings.block.php');
 
 	$labels = array(
-		'addPresetForm' => $theme->loadBlock('modules/lessons/views/addpresetform.block.php'),
 		'kalite.installation' => $kapenta->registry->get('kalite.installation'),
 		'kalite.admin.user' => $kapenta->registry->get('kalite.admin.user'),
 		'kalite.admin.pwd' => $kapenta->registry->get('kalite.admin.pwd'),
 		'kalite.db.file' => $kapenta->registry->get('kalite.db.file')
 	);
 
-	$html = $theme->replaceLabels($labels, $block); 
+	$html = $kapenta->theme->replaceLabels($labels, $block); 
 
 	return $html;
 }

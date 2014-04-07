8dd25729ab4e40ec92ed5f7717ad6ecc78f6e531
appiappelt@gmail.com <appiappelt@gmail.com@0147436e-aed1-11de-b8dd-ab4b6ce6d521>
2014-04-07 18:49:41 +0000

    new session variable handling

--- modules/lessons/actions/homekhan.act.php
+++ modules/lessons/actions/homekhan.act.php
@@ -6,42 +6,62 @@
 //*	This action starts the watch video do exercises functionality for KA Lite
 //--------------------------------------------------------------------------------------------------
 	global $kapenta;
-
+	
 	if ('admin' !== $kapenta->user->role 
 	and 'teacher' !== $kapenta->user->role
 	and 'student' !== $kapenta->user->role) { $kapenta->page->do403(); }
 
-	$sessionid = '';
-	$csrftoken = '';
 	$kalite = $kapenta->registry->get('kalite.installation');
+	
+	$cookies = "";
+	if (true == $kapenta->session->has('kalite_sessionid')) {
+		$sessionid = $kapenta->session->get('kalite_sessionid');
+		$cookies = 'sessionid='.$sessionid.';';
+	}
+	if (true == $kapenta->session->has('kalite_csrftoken')) {
+		$csrftoken = 	$kapenta->session->get('kalite_csrftoken');
+		$cookies = $cookies . 'csrftoken='.$csrftoken;
+	}
 	
 	//----------------------------------------------------------------------------------------------
 	//	check if user is already logged in into KA Lite, otherwise automatically create user (1st time) and log in
 	//----------------------------------------------------------------------------------------------
-	$url = 'http://localhost/api/status';
-	$reply = $kapenta->utils->curlGet($url, '', false);
+    $time = round(microtime(1) * 1000);	
+    $url = $kalite.'/api/status?_=' . $time;
+	$reply = $kapenta->utils->curlGet($url, "", false, $cookies);
+//	echo $reply;
 	if (0 < strpos($reply, '"is_logged_in": true')) {
-//	if (true == $kapenta->session->has('c_sessionid') and '' !== $kapenta->session->get('c_sessionid')) {
 		//signed in already, continue below
-		//echo "We are logged in with KhanLite already!<br/>\n";
+//		echo "We are already logged in with KhanLite!<br/>\n";
 	} else {
 //		echo "We are not logged in with KhanLite!<br/>\n";
 		logoutKhanLite();
-		createAndLoginKhanLite();
-	} 
-
+		$retarg = createAndLoginKhanLite();
+		$kapenta->session->set('kalite_sessionid', $retarg['sessionid']);
+		$kapenta->session->set('kalite_csrftoken', $retarg['csrftoken']);
+	}
 	//----------------------------------------------------------------------------------------------
 	//	call GET /watch and exercise from KA Lite Server
 	//----------------------------------------------------------------------------------------------
-	$sessionid = $kapenta->session->get('c_sessionid');
-	$raw = $kapenta->utils->curlGet($kalite, '', false, 'sessionid='.$sessionid);
+	if (true == $kapenta->session->has('kalite_sessionid')) {
+		$sessionid = $kapenta->session->get('kalite_sessionid');
+		$cookies = 'sessionid='.$sessionid.';';
+	}
+	if (true == $kapenta->session->has('kalite_csrftoken')) {
+		$csrftoken = 	$kapenta->session->get('kalite_csrftoken');
+		$cookies = $cookies . 'csrftoken='.$csrftoken;
+	}
+	$raw = $kapenta->utils->curlGet($kalite, '', false, $cookies);
 
 	//----------------------------------------------------------------------------------------------
 	//	remove internal KA Lite links so that we can control what functionality of KA Lite is called from Awarenet
 	//----------------------------------------------------------------------------------------------
 
-    $replaced = trimKAPage($raw);
+	$replaced = "";
+ 	$replaced = trimKAPage($raw);
 	$replaced = removeLinksFromKhanLitePage($replaced);
+	$replaced = replaceLinksFromKhanLitePage($replaced);
+	$replaced = changeLocalLinksFromKhanLitePage($replaced);
 
 	//----------------------------------------------------------------------------------------------
 	//	Render KA Lite sub page

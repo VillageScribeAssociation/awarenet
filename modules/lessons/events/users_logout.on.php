35b082169ea4172953184a87b830cdf7f07835bf
appiappelt@gmail.com <appiappelt@gmail.com@0147436e-aed1-11de-b8dd-ab4b6ce6d521>
2014-04-07 19:07:37 +0000

    rather just call the logout function

--- modules/lessons/events/users_logout.on.php
+++ modules/lessons/events/users_logout.on.php
@@ -4,25 +4,8 @@
 //-------------------------------------------------------------------------------------------------
 //|	fired when the user logs out.php
 //-------------------------------------------------------------------------------------------------
-//arg: method - http POST or GET [string]
-//arg: uri - uri of http call [string]
-//arg: query - parameters in http call [string]
-//arg: remoteAddr - address of caller [string]
-//arg: remotePort - port of caller [string]
-//arg: remotePort - args of POST [string]
-
 function lessons__cb_users_logout($args) {
-	global $kapenta;
-
-	$reply = $kapenta->utils->curlGet($kapenta->registry->get('kalite.installation').'/securesync/logout/', '', false, '');
-	$kapenta->session->set('c_sessionid', '');
-	$kapenta->session->set('c_csrftoken', '');	
-
-	echo $reply;
-	//----------------------------------------------------------------------------------------------
-	//	ok, done
-	//----------------------------------------------------------------------------------------------	
-	return true;
+	return logoutKhanLite();
 }
 
 //-------------------------------------------------------------------------------------------------

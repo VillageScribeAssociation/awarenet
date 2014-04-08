b2a84d3f7441cc3df539f14260cbf975bfdae481
appiappelt@gmail.com <appiappelt@gmail.com@0147436e-aed1-11de-b8dd-ab4b6ce6d521>
2014-04-08 07:35:36 +0000

    the menu for a kalite student

---
+++ modules/lessons/views/menukalitestudent.block.php
@@ -0,0 +1,5 @@
+<? /*
+
+[[:theme::submenu::label=KAlite Home::link=/lessons/kalite::alt=From Siyavula:]]
+[[:theme::submenu::label=Watch & Exercise::link=/lessons/homekhan::alt=Khan Academy Lite:]]
+*/ ?>

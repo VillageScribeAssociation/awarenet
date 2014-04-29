9234657db254c9cbea62e3defa1903d331726cda
appiappelt@gmail.com <appiappelt@gmail.com@0147436e-aed1-11de-b8dd-ab4b6ce6d521>
2014-04-29 12:03:24 +0000

    simplify/unify code

---
+++ modules/picturelogin/views/pictureloginpictures.block.php
@@ -0,0 +1,11 @@
+<? /*
+			<tr>
+				<td width='80px'><b>Pictures</b></td>
+				<td>
+					<div id='source' style="background-color:#FFD700" ondragstart="drag(event)" ondrop="dropSource(event)" 				
+						ondragover="allowDropSource(event)">
+						[[:picturelogin::aDataIconDefinition:]]
+					</div>
+				</td>
+			</tr>
+*/ ?>
---
+++ modules/picturelogin/views/pictureloginshort.block.php
@@ -0,0 +1,17 @@
+<? /*
+		<table noborder width='100%'>
+			<tr>
+				<td></td>
+				<td><h3>Compile your Picture Password</h3></td>
+			</tr>		
+			<tr>
+				<td width='80px'><b>Pictures</b></td>
+				<td>
+					<div id='source' style="background-color:#FFD700;height:185px;width:530px;float:left;" ondragstart="drag(event)" 							ondrop="dropSource(event)" ondragover="allowDropSource(event)">
+						[[:picturelogin::aDataIconDefinitionSmall:]]
+					</div>
+				</td>
+			</tr>
+			[[:picturelogin::pictureloginpassword:]]
+		</table>
+*/ ?>

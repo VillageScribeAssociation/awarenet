d44a39e8fa3bef1b194e4df672aa0b046fcf2fb3
appiappelt@gmail.com <appiappelt@gmail.com@0147436e-aed1-11de-b8dd-ab4b6ce6d521>
2014-04-29 12:00:46 +0000

    simplify/unify code

---
+++ modules/picturelogin/views/picturelogin.block.php
@@ -0,0 +1,31 @@
+<? /*
+		<table noborder width='100%'>
+			<tr>
+				<td></td>
+				<td><h3>Compile your Picture Password</h3></td>
+			</tr>		
+			[[:picturelogin::pictureloginpictures:]]
+			[[:picturelogin::pictureloginpassword:]]
+			<form name='siteLogin' method='POST' action='%%serverPath%%users/login/'>
+				<input type='hidden' name='action' value='login' />
+				<tr>	
+					<td></td>
+					<td><h3>awarenet Login Details</h3></td>
+				</tr>	
+			  	<tr>
+					<td width='80px'><b>Username</b></td>
+					<td><input id='txtUser' type='text' name='user' value='' size='20' style='width: 95%;' /></td>
+			  	</tr>
+			  	<tr>
+					<td width='80px'><b>Password</b></td>
+			  		<td>
+						<input type='password' name='pass' size='20' value='' style='width: 95%;' />		
+					</td>
+				</tr>
+			 	<tr>
+			  		<td><b>Login to awarenet</b></td>
+					<td><input type='submit' value='Log In' /></td>
+			  	</tr>
+			</form>
+		</table>
+*/ ?>

4938a19b6908a006d4b8f3fa7a5639766a15b4af
appiappelt@gmail.com <appiappelt@gmail.com@0147436e-aed1-11de-b8dd-ab4b6ce6d521>
2014-04-29 12:02:03 +0000

    simplify/unify code

---
+++ modules/picturelogin/views/pictureloginchange.block.php
@@ -0,0 +1,39 @@
+<? /*
+		<table noborder width='100%'>
+			<tr>
+				<td><b>User:</b></td>
+				<td><b><input type='text' id='txtUser' value='%%username%%' readonly style='width: 95%;' /></b></td>
+			</tr>
+			[[:picturelogin::pictureloginpictures:]]
+			<tr>
+				<td width='80px'><b>Generate Pictures</b></td>
+				<td>
+					<form onSubmit="return captureFormPics()">
+						<input type='submit' id='generatePics' value='Generate Pictures' />
+						[[:picturelogin::generatePicturesScript:]]
+					</form>
+				</td>
+		 	</tr>
+			[[:picturelogin::pictureloginpassword:]]
+			<form name='changeUserPass' method='POST' action='%%serverPath%%users/changepassword/'>
+			<input type='hidden' name='action' value='changeUserPass' />
+			<input type='hidden' name='UID' value='%%UID%%' />
+				<tr>
+					<td><b>Current Password:</b></td>
+					<td><b><input type='password' id='txtPassword' name='pwdCurrent' style='width: 95%;' /></b></td>
+				</tr>
+				<tr>
+					<td><b>New Password:</b></td>
+					<td><b><input type='password' name='pwdNew' style='width: 95%;' /></b></td>
+				</tr>
+				<tr>
+					<td><b>Confirm New Password:</b></td>
+					<td><b><input type='password' name='pwdConfirm'  style='width: 95%;' /></b></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td><b><input type='submit' value='Change Password' /></b></td>
+				</tr>
+			</form>
+		</table>
+*/ ?>

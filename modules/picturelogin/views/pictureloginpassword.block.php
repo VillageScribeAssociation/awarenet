f3c4d57ee97a4d3d4e4ac89f5ad0334ddedb3697
appiappelt@gmail.com <appiappelt@gmail.com@0147436e-aed1-11de-b8dd-ab4b6ce6d521>
2014-04-29 12:02:47 +0000

    simplify/unify code

---
+++ modules/picturelogin/views/pictureloginpassword.block.php
@@ -0,0 +1,41 @@
+<? /*
+			<tr>
+				<td width='80px'><b>Picture Password</b></td>
+				<td>
+					<div id="drop1" ondragstart="drag(event)" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+					<div id="drop2" ondragstart="drag(event)" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+					<div id="drop3" ondragstart="drag(event)" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+					<div id="drop4" ondragstart="drag(event)" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+					<div id="drop5" ondragstart="drag(event)" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+					<div id="drop6" ondragstart="drag(event)" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+					<div id="drop7" ondragstart="drag(event)" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+					<div id="drop8" ondragstart="drag(event)" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+					<div id="drop9" ondragstart="drag(event)" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+					<div id="drop10" ondragstart="drag(event)" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+					<div id="drop11" ondragstart="drag(event)" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+					<div id="drop12" ondragstart="drag(event)" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+					<div id="drop13" ondragstart="drag(event)" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+					<div id="drop14" ondragstart="drag(event)" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+					<div id="drop15" ondragstart="drag(event)" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+					<div id="drop16" ondragstart="drag(event)" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+					<div id="drop17" ondragstart="drag(event)" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+					<div id="drop18" ondragstart="drag(event)" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+					<div id="drop19" ondragstart="drag(event)" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+				</td>
+			</tr>
+			<tr>
+				<td width='80px'><b>Generate Password</b></td>
+				<td>
+					<form onSubmit="return captureForm()">
+						<input type='submit' id='generate'value='Generate' />
+						[[:picturelogin::generatePasswordScript:]]
+					</form>
+				</td>
+		 	</tr>
+			<tr>
+				<td width='80px'><b>Generated Password</b></td>
+				<td>
+					<input id='genpass' type='text' name='Generated Password' value='' size='20' style='width: 95%;' />
+				</td>
+			</tr>		
+*/ ?>

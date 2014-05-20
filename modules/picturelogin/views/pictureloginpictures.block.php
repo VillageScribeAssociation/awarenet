e2dd71584a2b6c721027e666eee6b253c18061a6
appiappelt@gmail.com <appiappelt@gmail.com@0147436e-aed1-11de-b8dd-ab4b6ce6d521>
2014-05-20 13:56:54 +0000

    remove ondragover DOM event. drag and drop is not implemented on document level

--- modules/picturelogin/views/pictureloginpictures.block.php
+++ modules/picturelogin/views/pictureloginpictures.block.php
@@ -2,8 +2,7 @@
 			<tr>
 				<td width='80px'><b>Pictures</b></td>
 				<td>
-					<div id='source' style="background-color:#FFD700" ondragstart="drag(event)" ondrop="dropSource(event)" 				
-						ondragover="allowDropSource(event)">
+					<div id='source' style="background-color:#FFD700">
 						[[:picturelogin::aDataIconDefinition:]]
 					</div>
 				</td>

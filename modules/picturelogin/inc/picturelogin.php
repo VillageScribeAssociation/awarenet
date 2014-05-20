8d5601d589b8e290389bd76be7cbcc4bd1bc7dd5
appiappelt@gmail.com <appiappelt@gmail.com@0147436e-aed1-11de-b8dd-ab4b6ce6d521>
2014-05-20 13:47:46 +0000

    move javascript code into dragdrop.js

--- modules/picturelogin/inc/picturelogin.php
+++ modules/picturelogin/inc/picturelogin.php
@@ -3,7 +3,6 @@
 //--------------------------------------------------------------------------------------------------
 //*	Functionality helping to integrate picturelogin into other pages
 //--------------------------------------------------------------------------------------------------
-
 //--------------------------------------------------------------------------------------------------
 //|	return the style string
 //--------------------------------------------------------------------------------------------------
@@ -53,84 +52,53 @@
 		{float:left; width:100%; height:126px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
 
 		#drop1
-		{float:left; width:30px; height:32px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
+		{float:left; width:35px; height:35px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
 		#drop2
-		{float:left; width:30px; height:32px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
+		{float:left; width:35px; height:35px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
 		#drop3
-		{float:left; width:30px; height:32px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
+		{float:left; width:35px; height:35px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
 		#drop4
-		{float:left; width:30px; height:32px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
+		{float:left; width:35px; height:35px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
 		#drop5
-		{float:left; width:30px; height:32px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
+		{float:left; width:35px; height:35px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
 		#drop6
-		{float:left; width:30px; height:32px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
+		{float:left; width:35px; height:35px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
 		#drop7
-		{float:left; width:30px; height:32px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
+		{float:left; width:35px; height:35px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
 		#drop8
-		{float:left; width:30px; height:32px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
+		{float:left; width:35px; height:35px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
 		#drop9
-		{float:left; width:30px; height:32px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
+		{float:left; width:35px; height:35px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
 		#drop10
-		{float:left; width:30px; height:32px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
+		{float:left; width:35px; height:35px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
 		#drop11
-		{float:left; width:30px; height:32px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
+		{float:left; width:35px; height:35px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
 		#drop12
-		{float:left; width:30px; height:32px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
+		{float:left; width:35px; height:35px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
 		#drop13
-		{float:left; width:30px; height:32px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
+		{float:left; width:35px; height:35px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
 		#drop14
-		{float:left; width:30px; height:32px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
+		{float:left; width:35px; height:35px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
 		#drop15
-		{float:left; width:30px; height:32px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
+		{float:left; width:35px; height:35px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
 		#drop16
-		{float:left; width:30px; height:32px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
+		{float:left; width:35px; height:35px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
 		#drop17
-		{float:left; width:30px; height:32px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
+		{float:left; width:35px; height:35px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
 		#drop18
-		{float:left; width:30px; height:32px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
+		{float:left; width:35px; height:35px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
 		#drop19
-		{float:left; width:30px; height:32px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
+		{float:left; width:35px; height:35px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
 		#drop20
-		{float:left; width:30px; height:32px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
+		{float:left; width:35px; height:35px; margin:1px;padding:1px;border:1px solid #aaaaaa;}
 	';	
 		return $style;
 	}
 
 	function getPictureLoginScript() {
-		$script = '<script>
-			function allowDrop(ev)
-			{
-				var id = ev.target.id;
-				if (-1 < id.search("drop") && 0 == ev.target.children.length) {
-					ev.preventDefault();
-	 			}
-			}
-
-			function allowDropSource(ev)
-			{
-				ev.preventDefault();
-			}
-
-			function drag(ev)
-			{
-				ev.dataTransfer.setData("Text",ev.target.getAttribute("id"));
-			}
-
-			function dropSource(ev)
-			{
-				ev.preventDefault();
-				var data=ev.dataTransfer.getData("Text");
-				document.getElementById("source").appendChild(document.getElementById(data));
-	 		}
-
-			function drop(ev)
-			{
-				ev.preventDefault();
-				var data=ev.dataTransfer.getData("Text");
-	 			ev.target.appendChild(document.getElementById(data));
-			}
-		</script>	
-		';
+		global $kapenta;
+		$script = '<script src="' . $kapenta->serverPath . 'modules/picturelogin/js/dragdrop.js">
+		</script>';
 		
 		return $script;
 	}

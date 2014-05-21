fcf6e99b08dddb354a2d9ef09f7a0bae73cc2014
appiappelt@gmail.com <appiappelt@gmail.com@0147436e-aed1-11de-b8dd-ab4b6ce6d521>
2014-05-21 14:03:50 +0000

    first checkin

---
+++ modules/ldaplogin/module.xml.php
@@ -0,0 +1,16 @@
+<? /*
+<?xml version="1.0" encoding="UTF-8" ?>
+<module>
+    <modulename>ldaplogin</modulename>
+    <version>0</version>
+    <revision>0</revision>
+    <description>automatic login functionality if ldap is present</description>
+    <core>no</core>
+    <dependencies>
+    </dependencies>
+    <models>
+    </models>
+    <defaultpermissions>
+    </defaultpermissions>
+</module>
+*/ ?>
---
+++ modules/ldaplogin/views/checklsuser.block.php
@@ -0,0 +1,27 @@
+<? /*
+		<form id='ldapLogin' method='POST' action='%%serverPath%%users/login/'>
+		<input type='hidden' name='action' value='login' />
+		<input type='hidden' id='ls_user' name='user' value='' size='20' style='width: 100%;' />
+		<input type='hidden' id='ls_pass' name='pass' size='20' value='' style='width: 100%;' />
+		<input type='hidden' value='Log In' />
+		</form>
+		
+	<script language='javascript'>
+		function supports_html5_storage() {
+		  try {
+			return 'localStorage' in window && window['localStorage'] !== null;
+		  } catch (e) {
+			return false;
+		  }
+		}
+		if(supports_html5_storage()){
+			var username = localStorage.getItem("username");
+			var password = localStorage.getItem("password");
+			document.getElementById("ls_user").value = username;
+			document.getElementById("ls_pass").value = password;
+			document.getElementById("ldapLogin").submit();
+		} else {
+			alert("this browser does not support local storage! Please use either Internet Explorer, Firefox, Opera or Chrome browser!");
+		}
+	</script>
+*/ ?>

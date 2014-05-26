9a6bbd06e9feefb9f5ab7f8a109cb3729d103f17
appiappelt@gmail.com <appiappelt@gmail.com@0147436e-aed1-11de-b8dd-ab4b6ce6d521>
2014-05-26 14:08:40 +0000

    include a check if kalite server is running before showing the kalite integration menu.

--- modules/lessons/actions/kalite.act.php
+++ modules/lessons/actions/kalite.act.php
@@ -11,24 +11,32 @@
 	and 'teacher' !== $kapenta->user->role
 	and 'student' !== $kapenta->user->role) { $kapenta->page->do403(); }
 
-	$contents = '<div id="logo">
-                 	<a id="logo-image-small"  title="KA Lite Home">
-                  		<img src="%%serverPath%%/kalite/static/images/horizontal-logo-small.png" alt="KA Lite logo">
-                        	</a>
-                            	</div>
-                            		<div class="clear"></div>
-                 </div>';
-                 
-    $contents = $contents . "<h1><a class='black' href='%%serverPath%%lessons/homekhan'>Watch Videos and Do Exercises</a></h1>
-							Watch Video Lessons and do the Exercises that the teacher has already prepared for you.";
+	$kalite = $kapenta->registry->get('kalite.installation');
+	$reply = $kapenta->utils->curlGet($kalite, '', true, '');
+	
+	if($reply){   
+		$contents = '<div id="logo">
+		             	<a id="logo-image-small"  title="KA Lite Home">
+		              		<img src="%%serverPath%%/kalite/static/images/horizontal-logo-small.png" alt="KA Lite logo">
+		                    	</a>
+		                        	</div>
+		                        		<div class="clear"></div>
+		             </div>';
+		             
+		$contents = $contents . "<h1><a class='black' href='%%serverPath%%lessons/homekhan'>Watch Videos and Do Exercises</a></h1>
+								Watch Video Lessons and do the Exercises that the teacher has already prepared for you.";
 							
-	if ('admin' == $kapenta->user->role
-		or 'teacher' == $kapenta->user->role) {
-   		$contents = $contents . "<h1><a class='black' href='%%serverPath%%lessons/coachreportskhan'>Coach Reports</a></h1>
-							Teacher, check the progress of your students with Khan Academy (KAlite) ";
-   		$contents = $contents . "<h1><a class='black' href='%%serverPath%%lessons/updatekhan'>Update Videos and Exercises</a></h1>
-							Teacher, download new Videos and Exercises from Khan Academy (KAlite) ";
-	}
+		if ('admin' == $kapenta->user->role
+			or 'teacher' == $kapenta->user->role) {
+	   		$contents = $contents . "<h1><a class='black' href='%%serverPath%%lessons/coachreportskhan'>Coach Reports</a></h1>
+								Teacher, check the progress of your students with Khan Academy (KAlite) ";
+	   		$contents = $contents . "<h1><a class='black' href='%%serverPath%%lessons/updatekhan'>Update Videos and Exercises</a></h1>
+								Teacher, download new Videos and Exercises from Khan Academy (KAlite) ";
+		}
+	} else {
+   		$contents = "<h1>Cannot access Kalite Server, please ask your Awarnet Administrator what the problem might be!</h1>";
+	} 
+	
 
 
 	//----------------------------------------------------------------------------------------------

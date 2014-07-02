267eb4f375521337b31e581900f5c9365f23896e
appiappelt@gmail.com <appiappelt@gmail.com@0147436e-aed1-11de-b8dd-ab4b6ce6d521>
2014-07-02 08:49:29 +0000

    add text and Button.jpg to summary of cMinds

--- modules/games/views/summary_cminds.block.php
+++ modules/games/views/summary_cminds.block.php
@@ -2,7 +2,7 @@
 
 <table noborder width='100%'>
   <tr>
-    <td valign='top' width='300px'>
+    <td valign='top' width='500px'>
 		<br/>
 		<a href='%%serverPath%%games/cminds/'>
 		<img src='%%serverPath%%modules/games/assets/cminds/preview300w.jpg' border='0' class='rounded'>
@@ -14,7 +14,13 @@
 		Eight different math and programming games are waiting for you. Can you think logically? Do you want to learn it? Then let’s get 			started!<p>
 
 		After the game has successfully loaded, click somewhere on the screen to enter the main menu.
-		<a href='%%serverPath%%games/cminds/'>[play]</a>
+		
+		<p>
+			Once the game has started, use these buttons in the top right corner to play the game or get further information.
+			<img src='%%serverPath%%modules/games/assets/cminds/Buttons.jpg' align='right' border='0' width='200' class='rounded'>
+
+		<p>
+			<a href='%%serverPath%%games/cminds/'>[play]</a>
     </td>
   </tr>
 </table>
